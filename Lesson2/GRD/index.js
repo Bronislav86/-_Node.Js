6ec19517685a143aeb2f96a24256e276070ec581
Bronislav <shevchenko-bg@yandex.ru>
2024-12-09 20:03:42 +0300

    was added test of GRD package

--- Lesson2/GRD/index.js
+++ Lesson2/GRD/index.js
@@ -1,39 +1,41 @@
-function generateRandomNumber(length=1) {
+function generateRandomNumber(length = 1) {
+	const min = Math.pow(10, length - 1);
+	const max = Math.pow(10, length) - 1;
 
-  const min = Math.pow(10, length - 1);
-  const max = Math.pow(10, length) - 1;
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function generateRandomDate(start, end) {
-  if (!(start instanceof Date) || !(end instanceof Date)) {
-      throw new Error("Both start and end must be valid Date objects.");
-  }
-  if (start > end) {
-      throw new Error("Start date must be before end date.");
-  }
+	if (!(start instanceof Date) || !(end instanceof Date)) {
+		throw new Error("Both start and end must be valid Date objects.");
+	}
+	if (start > end) {
+		throw new Error("Start date must be before end date.");
+	}
 
-  const randomTimestamp = Math.random() * (end.getTime() - start.getTime()) + start.getTime();
-  return new Date(randomTimestamp);
+	const randomTimestamp = Math.random() * (end.getTime() - start.getTime()) + start.getTime();
+	return new Date(randomTimestamp);
 }
 
 function generateRandomName() {
-  const firstNames = [
-      "John", "Jane", "Michael", "Emily", "David", "Sarah", 
-      "Chris", "Jessica", "Daniel", "Ashley"
-  ];
-  const lastNames = [
-      "Smith", "Johnson", "Brown", "Williams", "Jones", 
-      "Garcia", "Miller", "Davis", "Martinez", "Hernandez"
-  ];
+	const firstNames = ["John", "Jane", "Michael", "Emily", "David", "Sarah", "Chris", "Jessica", "Daniel", "Ashley"];
+	const lastNames = [
+		"Smith",
+		"Johnson",
+		"Brown",
+		"Williams",
+		"Jones",
+		"Garcia",
+		"Miller",
+		"Davis",
+		"Martinez",
+		"Hernandez",
+	];
 
-  const randomFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
-  const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+	const randomFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+	const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
 
-  return `${randomFirstName} ${randomLastName}`;
+	return `${randomFirstName} ${randomLastName}`;
 }
-
-console.log(generateRandomName());
 
 module.exports = { generateRandomNumber, generateRandomDate, generateRandomName };
---
+++ Lesson2/newProject/index.js
@@ -0,0 +1,3 @@
+const grd = require("grd_bronislav");
+
+console.log(grd.generateRandomNumber(10));
